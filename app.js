ac22dab27d88fb61dcdb85e436bbeadc6826e8b0
JooostS <162368781+JooostS@users.noreply.github.com>
2026-03-06 16:44:43 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,112 @@
+const statusText = document.getElementById("status")
+const modal = document.getElementById("modal")
+const title = document.getElementById("modal-title")
+const text = document.getElementById("modal-text")
+const closeBtn = document.getElementById("close")
+
+let triggered = {}
+
+closeBtn.onclick = () => {
+  modal.classList.add("hidden")
+}
+
+if(navigator.onLine){
+  alert("For best accuracy turn off Wi-Fi/Data so GPS is used.")
+}
+
+/* 
+ACCURACY SETTINGS
+--------------------------------
+Typical accuracy values:
+GPS: 3m – 15m
+WiFi: 20m – 50m
+Cell: 100m – 1000m
+*/
+
+const REQUIRED_ACCURACY = 25 // meters
+
+function distance(lat1, lon1, lat2, lon2){
+
+const R = 6371000
+
+const φ1 = lat1 * Math.PI/180
+const φ2 = lat2 * Math.PI/180
+const Δφ = (lat2-lat1) * Math.PI/180
+const Δλ = (lon2-lon1) * Math.PI/180
+
+const a =
+Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+Math.cos(φ1) * Math.cos(φ2) *
+Math.sin(Δλ/2) * Math.sin(Δλ/2)
+
+const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+
+return R * c
+}
+
+function showModal(location){
+
+title.textContent = location.name
+text.textContent = location.info
+
+modal.classList.remove("hidden")
+
+}
+
+function checkLocations(userLat,userLng){
+
+locations.forEach(loc=>{
+
+const d = distance(userLat,userLng,loc.lat,loc.lng)
+
+if(d < loc.radius && !triggered[loc.name]){
+
+triggered[loc.name] = true
+showModal(loc)
+
+}
+
+})
+
+}
+
+function success(position){
+
+const lat = position.coords.latitude
+const lng = position.coords.longitude
+const accuracy = position.coords.accuracy
+
+statusText.textContent =
+"Accuracy: " + Math.round(accuracy) + "m"
+
+if(accuracy > REQUIRED_ACCURACY){
+
+statusText.textContent += " (waiting for GPS lock...)"
+
+return
+}
+
+statusText.textContent =
+"GPS locked | Accuracy: " + Math.round(accuracy) + "m"
+
+checkLocations(lat,lng)
+
+}
+
+function error(err){
+statusText.textContent = "Location error: " + err.message
+}
+
+navigator.geolocation.watchPosition(
+success,
+error,
+{
+enableHighAccuracy: true,
+maximumAge: 0,
+timeout: 10000
+}
+)
+
+if("serviceWorker" in navigator){
+navigator.serviceWorker.register("service-worker.js")
+}
---
+++ locations.js
@@ -0,0 +1,19 @@
+const locations = [
+
+{
+name: "Joost home",
+lat: 52.076878024089766,
+lng: 4.748178044792492,
+radius: 20,
+info: "This square was once used for trading in the 1600s."
+},
+
+{
+name: "Jumbo",
+lat: 52.076832764754336,
+lng: 4.751099914917804,
+radius: 20,
+info: "This windmill was built in 1780 and restored in 1995."
+}
+
+];
